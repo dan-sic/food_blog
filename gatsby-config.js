b4b8139e68692ae478e1f90ba674062bf2eb6e18
Daniel_S <olgierd.dan@gmail.com>
2021-02-21 12:07:28 +0100

    Add base global styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,20 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Roboto",
+            },
+            {
+              family: "Lora",
+              variants: ["400", "700"],
+            },
+          ],
+        },
+      },
+    },
+  ],
 }
