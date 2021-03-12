9fad6abdce309705f35af0f2f5bf30cd00c962f4
Daniel_S <olgierd.dan@gmail.com>
2021-03-12 13:28:01 +0100

    Add PWA support

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,5 +49,29 @@ module.exports = {
         path: `${__dirname}/src/assets/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Food&Fun Blog`,
+        short_name: `Food&Fun`,
+        start_url: `/`,
+        background_color: `#fff3e6`,
+        theme_color: `#c1a1d3`,
+        display: `standalone`,
+        icon: `src/assets/logo-primary-small.svg`,
+        include_favicon: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [
+          `/index.html`,
+          "/contact/index.html",
+          "/articles/index.html",
+          "/404.html",
+        ],
+      },
+    },
   ],
 }
